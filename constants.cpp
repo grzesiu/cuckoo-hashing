893155e28cfe4981b1fb96da619485bedec63a6a
grzesiu <grzspychala@gmail.com>
2018-03-28 13:53:51 +0200

    Use constants in generate_dataset

--- constants.cpp
+++ constants.cpp
@@ -4,5 +4,5 @@
 
 const int Constants::filters_number = 5;
 const int Constants::dataset_size = 1000000;
-const int Constants::false_size = 100000;
+const int Constants::test_size = 100000;
 const int Constants::filter_size = filters_number * dataset_size / std::log(2);
--- constants.hpp
+++ constants.hpp
@@ -3,6 +3,6 @@ class Constants
     public:
         const static int filters_number;
         const static int dataset_size;
-        const static int false_size;
+        const static int test_size;
         const static int filter_size;
 };
