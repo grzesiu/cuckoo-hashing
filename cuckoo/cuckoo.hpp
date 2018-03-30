74935fc9112fb4f6558645b8f2a8c3e9856a8bf8
grzesiu <grzspychala@gmail.com>
2018-03-30 17:14:49 +0200

    Add iterator header

--- cuckoo/cuckoo.hpp
+++ cuckoo/cuckoo.hpp
@@ -1,8 +1,27 @@
+#include <algorithm>
+#include <cstddef>
+
 template <typename T>
 class Cuckoo
 {
   int size;
 
 public:
+  class iterator : public std::iterator<std::input_iterator_tag,
+                                        T, ptrdiff_t, const T *, const T &>
+
+  {
+
+  public:
+    const T &operator*() const;
+    const T *operator->() const;
+    iterator &operator++();
+    iterator operator++(int);
+    bool operator==(iterator const &other) const;
+    bool operator!=(iterator const &other) const;
+  };
+
   Cuckoo(int size);
+  iterator begin();
+  iterator end();
 };
